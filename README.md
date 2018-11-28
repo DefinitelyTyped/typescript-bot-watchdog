353a9eb3be681bd1d45726024ef2e25917541007
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-11-28 09:59:09 -0800

    Edit everything to watch typescript-bot instead

--- README.md
+++ README.md
@@ -1,47 +1,20 @@
-## Is types-publisher working right now?
+## Is typescript-bot working right now?
 
-If the badge below is green, then the average latency for the last 30
-PRs is below 10,000
-seconds:
+If the badge below is green, then typescript-bot has been active on
+DefinitelyTyped some time in the last two hours:
 
 [![Build Status](https://typescript.visualstudio.com/TypeScript/_apis/build/status/sandersn.types-publisher-watchdog)](https://typescript.visualstudio.com/TypeScript/_build/latest?definitionId=13)
 
-## What is it?
-
-types-publisher-watchdog reports how long it takes for types-publisher
-to publish packages to `@types` once a PR has been merged on
-Definitely Typed. Currently it prints the results to the terminal, but
-the intent is for it to run as a real watchdog that can issue alerts
-to the Typescript team members who work on Definitely Typed.
+typescript-bot-watchdog reports on recent activity by typescript-bot,
+but only on the DefinitelyTyped repo. The watchdog just checks the
+time of the first event from typescript-bot's feed.
 
 The watchdog pays attention to two kinds of data from DefinitelyTyped
 PRs: the date and the filenames. It could also use the declared
 version number from the header, but does not currently.
 
-## How it works
-
-1. Find the 30 most recently updated DefinitelyTyped PRs.
-2. Record the merge time of each PR, remembering to dedupe multiple
-   PRs to the same package.
-3. Find the publish time of each corresponding `@types` package.
-4. Compare the two times, using the current time if the package hasn't
-   published yet.
-5. Print the times, plus the average time across all packages.
-
-Eventually, I'll add:
-
-6. Raise an alert if the average time goes over some threshold &mdash;
-probably an hour or two.
 
 ## Limitations
 
-The watchdog is simple, and probably shouldn't get much more complex
-so that it doesn't end up with bugs. Currently, it only looks at
-changed file *names* from the PR, not file contents. And it only looks
-at files that changed at the top-level of a package. That means that
-updates to old versions are skipped. It also means that I don't
-inspect the DefinitelyTyped header so I can compare the npm version. I
-just assume that npm's latest matches the change. I think this is
-usually true, but it may not be.
-
-
+I haven't checked whether the activity feed orders events most recent
+first. It would be super weird if it didn't.
--- index.js
+++ index.js
@@ -1,101 +1,33 @@
-const sh = require('shelljs')
-const fs = require('fs')
 const Gh = require('@octokit/rest')
 var gh = new Gh()
 
 async function main() {
     gh.authenticate({
         type: "token",
-        token: process.env["TYPES_PUBLISHER_WATCHDOG_TOKEN"] || ""
+        token: process.env["TYPESCRIPT_BOT_WATCHDOG_TOKEN"] || ""
     })
-    const prs = await recentPrs()
-    const averageLatency = recentPackages(prs)
-    if (averageLatency > 10000) {
-        console.log("average types-publisher latency was over 10,000 seconds");
+    const mostRecent = await recentActivity()
+
+    if (!mostRecent) {
+        console.log("Couldn't find any recent activity for typescript-bot")
+        throw new Error()
+    }
+    console.log()
+    console.log()
+    console.log("Time since typescript-bot's last activity: " + (Date.now() - mostRecent.valueOf()) / 1000)
+    if ((Date.now() - mostRecent.valueOf()) > 7200000) {
+        console.log("typescript-bot hasn't been active in over 2 hours (7200 seconds)")
         throw new Error();
     }
-    // fs.writeFileSync('/home/nathansa/types-publisher-watchdog/latency.json', JSON.stringify(rows))
 }
 
-/**
- * 1. Only match paths that begin with types/
- * 2. Only match paths that end with index.d.ts; test changes don't cause a republish
- * 3. Capture the package name
- */
-const THIS_IS_FINE = /^types\/([^\/]+?)\/index.d.ts$/
-
-/** returns {Promise<Map<string, Date>>} */
-async function recentPrs() {
-    const search = await gh.search.issues({
-        q: "is:pr is:merged repo:DefinitelyTyped/DefinitelyTyped",
-        order: "desc",
-        per_page: 30,
-        page: 1
-    })
-    /** @type {Map<string, { mergeDate: Date, pr: number }>} */
-    const prs = new Map()
-    for (const it of search.data.items) {
-        const mergeDate = (await gh.pulls.get({
-            owner: "DefinitelyTyped",
-            repo: "DefinitelyTyped",
-            number: it.number
-        })).data.merged_at
-        if (mergeDate == null)
-            continue
-        const fileEntries = (await gh.pulls.listFiles({
-            owner: "DefinitelyTyped",
-            repo: "DefinitelyTyped",
-            number: it.number,
-            per_page: 100,
-        })).data
-        /** @type {Set<string>} */
-        const mini = new Set()
-        for (const fileChange of fileEntries) {
-            const m = fileChange.filename.match(THIS_IS_FINE)
-            if (m == null)
-                continue
-            mini.add(m[1])
-        }
-        for (const name of mini) {
-            const date = new Date(mergeDate)
-            const prev = prs.get(name)
-            if (!prev || date > prev.mergeDate) {
-                prs.set(name, { mergeDate: date, pr: it.number })
-            }
+/** returns {Promise<Date>} */
+async function recentActivity() {
+    const events = await gh.activity.listEventsForUser({ username: 'typescript-bot' })
+    for (const event of events.data) {
+        if (event.repo.name === 'DefinitelyTyped/DefinitelyTyped') {
+            return new Date(event.created_at)
         }
     }
-    return prs
-}
-/** @param {Map<string, { mergeDate: Date, pr: number }>} prs */
-function recentPackages(prs) {
-    /** @type {Array<[string, number]>} */
-    let latencies = []
-    console.log()
-    console.log()
-    console.log("## Unpublished PRs ##")
-    for (const [name, { mergeDate, pr }] of prs) {
-        const publishDate = new Date(sh.exec(`npm info @types/${name} time.modified`, { silent : true }).stdout.trim())
-        if (mergeDate > publishDate) {
-            console.log(`${name}: #${pr} not published yet; latency so far: ${(Date.now() - mergeDate.valueOf()) / 1000}`)
-            console.log('       merged:' + mergeDate)
-            console.log('    published:' + publishDate)
-            latencies.push([name, Date.now() - mergeDate.valueOf()])
-        }
-        else {
-            latencies.push([name, publishDate.valueOf() - mergeDate.valueOf()])
-        }
-    }
-    latencies.sort(([_n1, l1], [_n2, l2]) => l1 === l2 ? 0 : l1 < l2 ? -1 : 1)
-    let sum = 0
-    console.log()
-    console.log()
-    console.log("## Publish latency ##")
-    for (const [name, latency] of latencies) {
-        sum += latency
-        console.log(name + ': ' + (latency / 1000))
-    }
-    console.log()
-    console.log('Average: ' + (sum / latencies.length / 1000))
-    return sum / latencies.length / 1000
 }
 main().catch(_ => process.exit(1))
